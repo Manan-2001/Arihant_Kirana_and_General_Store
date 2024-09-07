611fc2995caea511240afb53cf4f36b9c510f98d
Manan-tutorology <mananjain.tutorology@gmail.com>
2024-09-07 13:18:26 +0530

    Created Component and user side and admin side

---
+++ src/app/Admin/add-item-page/add-item-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-item-page.component.html',
+  styleUrl: './add-item-page.component.scss'
+})
+export class AddItemPageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AddItemPageComponent } from './Admin/add-item-page/add-item-page.component';
+import { ItemPageComponent } from './User/item-page/item-page.component';
+import { CartPageComponent } from './User/cart-page/cart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-item",
+        component:AddItemPageComponent
+    },
+    {
+        path:"items",
+        component:ItemPageComponent
+    },
+    {
+        path:"cart",
+        component:CartPageComponent
+    }
+];
